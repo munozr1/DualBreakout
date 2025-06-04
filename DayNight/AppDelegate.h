fcf807155c58750332112414ef4f8d9cfeb62f7e
Rodrigo Munoz <rm117579@gmail.com>
2025-06-03 20:46:08 -0500

    Initial Commit

---
+++ DayNight/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  DayNight
+//
+//  Created by Rodrigo Munoz on 6/3/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
